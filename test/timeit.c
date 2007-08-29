504f05d11ba2e1dcb90d89352edff9473a3cb893
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2007-08-29 14:42:29 +0000

    * fixed
    git-svn-id: file:///mnt/data/SVNrepo@1184 a13746d7-5020-4d46-8679-245a07f888a7

--- test/timeit.c
+++ test/timeit.c
@@ -1,4 +1,4 @@
-/* $Id: timeit.c,v 1.2 2007-08-29 14:18:09 wojtek Exp $ */
+/* $Id: timeit.c,v 1.3 2007-08-29 14:42:29 wojtek Exp $ */
 #include <sys/time.h>
 #include <time.h>
 
@@ -7,13 +7,13 @@
 #endif
 
 struct timeval t1, t2;
-int n, i, time1, time2, dt, best=1000000000;
+int k, i, time1, time2, dt, best=1000000000;
 
 #define TIMEIT(expression)					\
 	for (i=0; i < MAXITERS; i++) {				\
 		gettimeofday(&t1, NULL);			\
-		n = cnt;					\
-		while (n--)					\
+		k = cnt;					\
+		while (k--)					\
 			{expression;}				\
 		gettimeofday(&t2, NULL);			\
 		time1 = 1000000*t1.tv_sec + t1.tv_usec;		\
