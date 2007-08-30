48d1d7df2a6d4dbbc8cc59d7c101919cb18dc75b
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2007-08-30 20:56:15 +0000

    * added prototypes of unsafe functions
    git-svn-id: file:///mnt/data/SVNrepo@1194 a13746d7-5020-4d46-8679-245a07f888a7

--- src/sse2string.h
+++ src/sse2string.h
@@ -2,7 +2,7 @@
  *  SSE2 string routines library
  *  header file
  *
- *  $Revision: 1.2 $, $Date: 2007-08-29 14:45:44 $
+ *  $Revision: 1.3 $, $Date: 2007-08-30 20:56:15 $
  *
  *  Author: Wojciech Mu³a
  *  e-mail: wojciech_mula@poczta.onet.pl
@@ -34,6 +34,10 @@ extern int	sse2_strcmp(const char* s1, const char* s2) __pure__;
 
 /* unsafe string.h functions, i.e. assert aligned pointers and
    do not prevent reads beyond program's address space */
+extern size_t	sse2_strlen_unsafe(const char* s) __pure__;
+extern char*	sse2_strchr_unsafe(const char* s, int c) __pure__;
+extern char*	sse2_strrchr_unsafe(const char* s, int c) __pure__;
+extern int	sse2_strcmp_unsafe(const char* s1, const char* s2) __pure__;
 extern int	sse2_strncmp_unsafe(const char* s1, const char* s2, size_t n) __pure__;
 
 #endif
